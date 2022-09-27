00c36ecc3508a2375e4135ebcc27ac3b22fb5c79
Earthunes <olamidedavies001@gmail.com>
2022-09-27 21:40:05 +0100

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _memcpy - copies a memory area
- * @dest: memory area to be copied to 
+ * @dest: memory area to be copied to
  * @src: memory area to be copied from
  * @n: number of bytes to be copied
  *
@@ -11,12 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-for (; i < n; i++)
-{
-dest[i]  = src[i];
-}
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-return (dest);
+	return (dest);
 }
