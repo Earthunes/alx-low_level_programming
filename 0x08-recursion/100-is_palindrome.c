fa2ebe39cb128f4f885ea55c138e3a9855a5b866
Earthunes <olamidedavies001@gmail.com>
2022-09-30 02:47:12 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,8 +27,8 @@ void check(char *s, int start, int end, int *flag)
 {
 	if (start <= end)
 	{
-		if (s[start] ==s[end])
-			*flag *=1;
+		if (s[start] == s[end])
+			*flag *= 1;
 		else
 			*flag *= 0;
 		check(s, start + 1, end - 1, flag);
